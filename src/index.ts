15c5bca5359a6de234a3bda4615384295b65e697
zafkielkurumi <563693600@qq.com>
2021-09-22 16:53:51 +0800

    Feat::sparkles:  添加Command

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
 #!/usr/bin/env node
 
-import * as inquirer from "inquirer";
-import { spawn, execFileSync,exec } from "child_process";
-import * as path from "path";
+import inquirer from "inquirer";
+
 import { COMMIT_TYPE_CHOICES } from "./constant";
 import { Command } from "commander";
 import { getEmojis } from "./util/getEmojis";
-import emojisCache, { CACHE_PATH } from "./util/emojisCache";
-import execa from 'execa';
+import execa from "execa";
 
-  // const commit = new Command("commit");
-  // commit
-  //   .option("-s, --skip", "skip cache emoji, fetch gitemoji")
-  //   .action((args) => {
-  //     console.log(args);
-  //     main();
-  //     // inquirerPrompt();
-  //   });
+function main() {
+  const commit = new Command();
+  commit
+    .option("-s, --skip", "skip cache emoji, fetch gitemoji", false)
+    .parse()
+    const opts = commit.opts();
+    gitCommit(opts.skip);
+}
 
-async function main() {
-
-  const res: any[] = await getEmojis();
+async function gitCommit(skipCache: boolean) {
+  const res: any[] = await getEmojis(skipCache);
   const EMOJI_CHOICES = [
     {
-      name: "默认",
+      name: "无",
       value: "",
     },
-    ...res.map(r => ({ name: `${r.emoji}${r.name} - ${r.description}`, value: r.code})),
+    ...res.map((r) => ({
+      name: `${r.emoji}${r.name} - ${r.description}`,
+      value: r.code,
+    })),
   ];
-  inquirerPrompt(EMOJI_CHOICES)
+  inquirerPrompt(EMOJI_CHOICES);
 }
 
 async function inquirerPrompt(EMOJI_CHOICES) {
@@ -70,49 +70,18 @@ async function inquirerPrompt(EMOJI_CHOICES) {
           r.subject
         }`
       );
-      const [type, emoji] = (r.type as string).split('-');
-      const emojis = [emoji, ...r.emoji].join(' ');
-      console.log(path.join(__dirname, "./commit.sh"))
-      // execFileSync(path.join(__dirname, "./commit.sh"), {
-      //   stdio: 'inherit'
-      // });
-      const scope = r.scope? `(${r.scope})` : '';
-      // await execa('git', ['commit', '-m', r.type], {
-      //   stdio: "inherit",
-      //   buffer: false,
-      // });
-      await execa('git', ['commit', '-m', `${type}${scope}:${emojis} ${r.subject}`], {
-        stdio: "inherit",
-        buffer: false,
-      });
-      // exec("bash", [
-      //   path.join(__dirname, "./commit.sh"),
-      //   type,
-      //   r.scope,
-      //   r.subject,
-      //   emojis,
-      //   r.option
-      // ]);
+      const [type, emoji] = (r.type as string).split("-");
+      const emojis = [emoji, ...r.emoji].join(" ");
+      const scope = r.scope ? `(${r.scope})` : "";
+      await execa(
+        "git",
+        ["commit", "-m", `${type}${scope}:${emojis} ${r.subject}`],
+        {
+          stdio: "inherit",
+          buffer: false,
+        }
+      );
     });
 }
-
-// function exec(cmd: string, args: any[]) {
-//   return new Promise((resolve, reject) => {
-//     console.log(`Started: ${cmd} ${args.join(" ")}`);
-//     const app = spawn(cmd, args, { stdio: "inherit" });
-//     app.on("close", (code) => {
-//       if (code !== 0) {
-//         const err: any = new Error(`Invalid status code: ${code}`);
-//         err.code = code;
-//         return reject(err);
-//       }
-//       return resolve(code);
-//     });
-//     app.on("error", reject);
-//   });
-// }
-
-
-
 
 main();
--- src/util/getEmojis.ts
+++ src/util/getEmojis.ts
@@ -1,44 +1,44 @@
 /**
  *  from gitmoji cli
  */
-import chalk from 'chalk'
-import axios from 'axios'
-import ora from 'ora'
-import ProxyAgent from 'proxy-agent'
+import ora from "ora";
+import ProxyAgent from "proxy-agent";
+import fetch from 'node-fetch';
+import cache from "./emojisCache";
 
-import cache from './emojisCache'
-
- const GITMOJIS_URL = 'https://gitmoji.dev/api/gitmojis'
+const GITMOJIS_URL = "https://gitmoji.dev/api/gitmojis";
 
 export const getEmojis = async (
   skipCache: boolean = false
 ): Promise<Array<Object>> => {
-  const emojisFromCache = cache.getEmojis()
+  const emojisFromCache = cache.getEmojis();
+  if (cache.isAvailable() && emojisFromCache.length && !skipCache)
+    return emojisFromCache;
 
-  if (cache.isAvailable() && !skipCache) return emojisFromCache
-
-  const spinner = ora('Fetching gitmojis').start()
+  const spinner = ora("Fetching gitmojis").start();
   try {
-    const agent = new ProxyAgent(process.env.https_proxy || process.env.http_proxy || undefined);
-    const response = await axios.get(GITMOJIS_URL, {httpAgent: agent, httpsAgent: agent});
-    const data: any =  response.data;
-    const emojis = data.gitmojis;
-    console.log(emojis, 'emojis')
+    const agent = new ProxyAgent(
+      process.env.https_proxy || process.env.http_proxy || undefined
+    );
+    const response = await fetch(GITMOJIS_URL, {agent})
 
-    cache.createEmojis(emojis)
+    const data: any = await response.json();
+    const emojis = data.gitmojis;
+
+    cache.createEmojis(emojis);
 
     if (emojis.length === emojisFromCache.length) {
-      spinner.info('Gitmojis already up to date')
+      spinner.info("Gitmojis already up to date");
 
-      return []
+      return [];
     }
 
-    spinner.succeed('Gitmojis fetched successfully, these are the new emojis:')
+    spinner.succeed("Gitmojis fetched successfully, these are the new emojis:");
 
-    return emojis.filter((emoji) => !emojisFromCache.includes(emoji))
+    return emojis.filter((emoji) => !emojisFromCache.includes(emoji));
   } catch (error) {
-    spinner.fail(`Error: ${error}`)
+    spinner.fail(`Error: ${error}`);
 
-    return []
+    return [];
   }
-}
+};
